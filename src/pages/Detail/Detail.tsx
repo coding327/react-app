7781a940d4b04f1bd31590ff9a8b16a8850be22f
black jack <1545721718@qq.com>
2023-02-24 21:51:11 +0800

    feat: 补充

--- src/pages/Detail/Detail.tsx
+++ src/pages/Detail/Detail.tsx
@@ -1,34 +1,29 @@
+import { Ajax } from "@/api/api";
+import MyHead from "@/components/MyHead";
+import React, { FC, useEffect, useState } from "react";
+import { useLocation, useParams, useSearchParams } from "umi";
 
-
-
-import { Ajax } from '@/api/api'
-import MyHead from '@/components/MyHead'
-import React, { FC, useEffect , useState } from 'react'
-import { useLocation, useParams, useSearchParams } from 'umi'
-
-const Detail:FC = () => {
-    const location = useLocation()
-    const [query] = useSearchParams()
-    const params = useParams()
-    const [obj,setObj ] = useState<any>(null)
-    const getDetailData = async ()=>{
-        let res:any = await Ajax.gettravelbyid({
-            _id:params.id 
-        })
-        if(res.code==200){
-            setObj(res.result)
-        }
+const Detail: FC = () => {
+  const location = useLocation();
+  const [query] = useSearchParams();
+  const params = useParams();
+  const [obj, setObj] = useState<any>(null);
+  const getDetailData = async () => {
+    let res: any = await Ajax.gettravelbyid({
+      _id: params.id,
+    });
+    if (res.code == 200) {
+      setObj(res.result);
     }
-    useEffect(()=>{
-        getDetailData()
-    },[])
-    return (
-        <div>
-            <MyHead title={query.get('title')} ></MyHead>
+  };
+  useEffect(() => {
+    getDetailData();
+  }, []);
+  return (
+    <div>
+      <MyHead title={query.get("title")}></MyHead>
+    </div>
+  );
+};
 
-            
-        </div>
-    )
-}
-
-export default Detail
+export default Detail;
